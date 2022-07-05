d25755f20753a3b6fdd9252c263a8ed987c3b508
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-04 23:30:48 -0300

    Feat: modify HTML structure

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,9 +1,31 @@
 import { ReactNode } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 interface NavigationBarProps {
     children?: ReactNode;
 }
 
 export function NavigationBar() {
-    return <div>NavigationBar</div>;
+    return (
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src="" alt="Logo image" />
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+
+                <Navbar.Collapse id="basic-navbar-nav">
+                    {/* links list */}
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
