2ecce37fe334a50c3d496540347ac8f33877cce7
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-04 23:49:22 -0300

    Feat: using activeLink state to dynamic classes

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -37,9 +37,36 @@ export function NavigationBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     {/* links list */}
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link
+                            href="#home"
+                            className={
+                                activeLink === "home"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#skills"
+                            className={
+                                activeLink === "skills"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                        >
+                            Skills
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#projects"
+                            className={
+                                activeLink === "projects"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                        >
+                            Projects
+                        </Nav.Link>
                     </Nav>
 
                     <span className="navbar-text">
